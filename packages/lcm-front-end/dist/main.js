3cf1ba3f7f783ba5423bba21f80a61b806eb1b2e
Dũng Nguyễn <qngnud@gmail.com>
2021-02-03 22:31:06 +0700

    Initial commit

---
+++ packages/lcm-front-end/dist/main.js
@@ -0,0 +1,15 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/*!***********************!*\
+  !*** ./src/index.tsx ***!
+  \***********************/
+eval("throw new Error(\"Module parse failed: Unexpected token (31:51)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n| \\n| const store = configureAppStore();\\n> const MOUNT_NODE = document.getElementById('root') as HTMLElement;\\n| \\n| ReactDOM.render(\");\n\n//# sourceURL=webpack://lcm-front-end/./src/index.tsx?");
+/******/ })()
+;
