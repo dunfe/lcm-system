4c6d7f9af9edcf7bc62fc4cb95f7c3246e5e8213
AnhTN <cachepdibao13@gmail.com>
2021-02-08 15:41:21 +0700

    fix style coding

--- packages/lcm-back-end/controller/user.js
+++ packages/lcm-back-end/controller/user.js
@@ -2,7 +2,7 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import User from '../models/user.js';
 import config from '../config/config.js';
-import user from '../models/user.js';
+
 
 const router = express.Router();
 
