77106f4c479de5062b5bfeb587855012ce45d2c6
AnhTN <cachepdibao13@gmail.com>
2021-03-09 22:42:35 +0700

    add dashboard api

--- packages/api/controller/admin.js
+++ packages/api/controller/admin.js
@@ -6,6 +6,21 @@ const router = express.Router();
 
 const ObjectId = mongoose.Types.ObjectId;
 
-
+export const  totalUser = (req, res) => {
+    User.aggregate([
+        {
+            $match:{}
+        },
+        {
+            $count: "total_user"
+        }
+    ], (err, doc) => {
+        if (!err){ 
+            res.json(doc);
+        } else {
+            console.log('Error' + JSON.stringify(err, undefined, 2));
+        };
+    });
+};
 
 export default router;
