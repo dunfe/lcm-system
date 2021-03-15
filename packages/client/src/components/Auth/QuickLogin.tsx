09bddd1881af5038f3e41111187ecd98561c8864
Dũng Nguyễn <qngnud@gmail.com>
2021-03-15 14:26:14 +0700

    update admin port

--- packages/client/src/components/Auth/QuickLogin.tsx
+++ packages/client/src/components/Auth/QuickLogin.tsx
@@ -4,41 +4,19 @@ import Icon from '@ant-design/icons';
 import { Facebook } from '../../components/Logo/Facebook';
 import { Goolge } from '../../components/Logo/Google';
 import { Github } from '../../components/Logo/Github';
-import { useAuth } from '../../utils/hooks/useAuth';
-import { message } from 'antd';
-import { useHistory } from 'react-router-dom';
 
 const QuickLogin = () => {
-    const auth = useAuth();
-    const history = useHistory();
 
-    const handleGoogleClick = () => {
-        auth
-        .signInWithGoogle()
-        .then(response => {
-  
-          if (response) {
-            message.success('Đăng nhập thành công!').then(() => {
-              console.log(response);
-            });
-  
-            history.push("/");
-          }
-        })
-        .catch(error => {
-          message.error('Đăng nhập không thành công!').then(() => {
-            console.error(error);
-          });
-        });
-    }
-    return (
-        <QuickLoginContainer>
-            <span>Đăng nhập nhanh: </span>
-            <Icon component={Facebook} />
-            <Icon component={Goolge} onClick={handleGoogleClick}/>
-            <Icon component={Github} />
-        </QuickLoginContainer>
-    )
+  return (
+    <QuickLoginContainer>
+      <span>Đăng nhập nhanh: </span>
+      <Icon component={Facebook} />
+      <a href="http://localhost:3000/api/users/google">
+        <Icon component={Goolge} />
+      </a>
+      <Icon component={Github} />
+    </QuickLoginContainer>
+  )
 };
 
 const QuickLoginContainer = styled.div`
