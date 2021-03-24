f6efac826c75df5d0f87a91cb2d0aa35df72106b
Dung <qngnud@gmail.com>
2021-03-24 22:11:47 +0700

    update config

--- packages/client/src/components/Auth/QuickLogin.tsx
+++ packages/client/src/components/Auth/QuickLogin.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import styled from 'styled-components';
 import Icon from '@ant-design/icons';
 import { Facebook } from '../../components/Logo/Facebook';
-import { Goolge } from '../../components/Logo/Google';
+import { Google } from '../../components/Logo/Google';
 import { Github } from '../../components/Logo/Github';
 
 const QuickLogin = () => {
@@ -10,11 +10,11 @@ const QuickLogin = () => {
   return (
     <QuickLoginContainer>
       <span>Đăng nhập nhanh: </span>
-      <a href="http://localhost:3000/api/users/facebook">
+      <a href="https://localhost:3000/api/users/facebook">
         <Icon component={Facebook} />
       </a>
       <a href="http://localhost:3000/api/users/google">
-        <Icon component={Goolge} />
+        <Icon component={Google} />
       </a>
       <a href="http://localhost:3000/api/users/github">
         <Icon component={Github} />
