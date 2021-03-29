b45e1b409318fef0c4d75ad94b58f3ec304e2d59
Dung <qngnud@gmail.com>
2021-03-29 10:29:39 +0700

    update react-router

--- packages/admin/src/pages/Home/Home.tsx
+++ packages/admin/src/pages/Home/Home.tsx
@@ -1,84 +1,77 @@
 import * as React from 'react';
-import { Layout, Menu } from 'antd';
-import {DashboardOutlined, FormOutlined, TeamOutlined, CheckCircleOutlined
+import {Layout, Menu} from 'antd';
+import {
+    DashboardOutlined, FormOutlined, TeamOutlined, CheckCircleOutlined
 } from '@ant-design/icons';
 import HeaderComponent from '../../components/Header/Header';
 import {
-  BrowserRouter,
-  Link,
-  Route,
-  Switch,
-  useRouteMatch,
+    Switch,
+    Link,
+    Route, useRouteMatch,
 } from 'react-router-dom';
 import Skills from "../../components/Manage/Skills";
 
-const { Sider, Content } = Layout;
+const {Sider, Content} = Layout;
 
 export function HomePage() {
-  //check login
-  const { path } = useRouteMatch();
+    //check login
+    const {path, url} = useRouteMatch();
 
-  return (
-    <>
-      <Layout style={{ height: '100vh' }}>
-        <BrowserRouter basename={"/"}>
-          <HeaderComponent />
-          <Layout>
-            <Sider
-              breakpoint="lg"
-              collapsedWidth="0"
-              onCollapse={(collapsed, type) => {
-                console.log(collapsed, type);
-              }}
-              theme="light"
-            >
-              <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
-                <Menu.Item key="1" icon={<DashboardOutlined />}>
-                  <Link to={`/`}>Dashboard</Link>
-                </Menu.Item>
-                <Menu.Item key="2" icon={<FormOutlined />}>
-                  <Link to={`/skills`}>Quản lí kỹ năng</Link>
-                </Menu.Item>
-                <Menu.Item key="3" icon={<TeamOutlined />}>
-                  <Link to={`/mentees`}>Quản lí Mentee</Link>
-                </Menu.Item>
-                <Menu.Item key="4" icon={<TeamOutlined />}>
-                  <Link to={`/mentors`}>Quản lí Mentor</Link>
-                </Menu.Item>
-                <Menu.Item key="5" icon={<CheckCircleOutlined />}>
-                  <Link to={`/feedbacks`}>Quản lí Feedback</Link>
-                </Menu.Item>
-              </Menu>
-            </Sider>
+    return (
+        <Layout style={{height: '100vh'}}>
+            <HeaderComponent/>
             <Layout>
-              <Content style={{ margin: '24px 16px 0' }}>
-                <div
-                  className="site-layout-background"
-                  style={{ padding: 24, minHeight: 360, backgroundColor: "white" }}
+                <Sider
+                    breakpoint="lg"
+                    collapsedWidth="0"
+                    onCollapse={(collapsed, type) => {
+                        console.log(collapsed, type);
+                    }}
+                    theme="light"
                 >
-                  <Switch>
-                    <Route exact path={path}>
-                      <h3>Dashboard</h3>
-                    </Route>
-                    <Route path={`/skills`}>
-                      <Skills />
-                    </Route>
-                    <Route path={`/mentees`}>
-                      <h3>Quản lí Mentee</h3>
-                    </Route>
-                    <Route path={`/mentors`}>
-                      <h3>Quản lí Mentor</h3>
-                    </Route>
-                    <Route path={`/feedbacks`}>
-                      <h3>Quản lí Feedback</h3>
-                    </Route>
-                  </Switch>
-                </div>
-              </Content>
+                    <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1" icon={<DashboardOutlined/>}>
+                            <Link to={`/`}>Dashboard</Link>
+                        </Menu.Item>
+                        <Menu.Item key="2" icon={<FormOutlined/>}>
+                            <Link to={`/skills`}>Quản lí kỹ năng</Link>
+                        </Menu.Item>
+                        <Menu.Item key="3" icon={<TeamOutlined/>}>
+                            <Link to={`/mentees`}>Quản lí Mentee</Link>
+                        </Menu.Item>
+                        <Menu.Item key="4" icon={<TeamOutlined/>}>
+                            <Link to={`/mentors`}>Quản lí Mentor</Link>
+                        </Menu.Item>
+                        <Menu.Item key="5" icon={<CheckCircleOutlined/>}>
+                            <Link to={`/feedbacks`}>Quản lí Feedback</Link>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Content style={{margin: '24px 16px 0'}}>
+                        <div
+                            className="site-layout-background"
+                            style={{padding: 24, minHeight: 360, backgroundColor: "white"}}
+                        >
+                            <Switch>
+                                <Route exact path={`${path}`}>
+                                    <h3>Dashboard</h3>
+                                </Route>
+                                <Route path={`/skills`} component={Skills}/>
+                                <Route path={`/mentees`}>
+                                    <h3>Quản lí Mentee</h3>
+                                </Route>
+                                <Route path={`/mentors`}>
+                                    <h3>Quản lí Mentor</h3>
+                                </Route>
+                                <Route path={`/feedbacks`}>
+                                    <h3>Quản lí Feedback</h3>
+                                </Route>
+                            </Switch>
+                        </div>
+                    </Content>
+                </Layout>
             </Layout>
-          </Layout>
-        </BrowserRouter>
-      </Layout>
-    </>
-  );
+        </Layout>
+    );
 }
