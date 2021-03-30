e442dade59cfb6042fa90d54c55f788875f3dc78
Dung <qngnud@gmail.com>
2021-03-30 16:44:39 +0700

    add dashboard

--- packages/admin/src/features/dashboard/Dashboard.tsx
+++ packages/admin/src/features/dashboard/Dashboard.tsx
@@ -1,6 +1,9 @@
-import {Row, Col, Card} from "antd";
+import {Row, Col, Card, Table, Avatar, List} from "antd";
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import * as React from "react";
-import "./Dashboard.css"
+import "./Dashboard.css";
+
+const {Meta} = Card;
 
 const Dashboard = () => {
     return (
@@ -27,6 +30,69 @@ const Dashboard = () => {
                     </Card>
                 </Col>
             </Row>
+            <Card title="Danh sách câu hỏi" bordered={false} style={{marginTop: 24, marginBottom: 24}}>
+                <Table  dataSource={[]}/>
+            </Card>
+            <Card title="Danh sách mentor" bordered={false} style={{marginTop: 24, marginBottom: 24}}>
+                <List
+                    grid={{ gutter: 16, column: 4 }}
+                    dataSource={[]}
+                    renderItem={() => (
+                        <List.Item>
+                            <Card
+                                style={{ width: 300 }}
+                                cover={
+                                    <img
+                                        alt="example"
+                                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                                    />
+                                }
+                                actions={[
+                                    <SettingOutlined key="setting" />,
+                                    <EditOutlined key="edit" />,
+                                    <EllipsisOutlined key="ellipsis" />,
+                                ]}
+                            >
+                                <Meta
+                                    avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                    title="Card title"
+                                    description="This is the description"
+                                />
+                            </Card>
+                        </List.Item>
+                    )}
+                />
+            </Card>
+            <Card title="Danh sách mentee" bordered={false} style={{marginTop: 24, marginBottom: 24}}>
+                <List
+                    grid={{ gutter: 16, column: 4 }}
+                    dataSource={[]}
+                    renderItem={() => (
+                        <List.Item>
+                            <Card
+                                style={{ width: 300 }}
+                                cover={
+                                    <img
+                                        alt="example"
+                                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                                    />
+                                }
+                                actions={[
+                                    <SettingOutlined key="setting" />,
+                                    <EditOutlined key="edit" />,
+                                    <EllipsisOutlined key="ellipsis" />,
+                                ]}
+                            >
+                                <Meta
+                                    avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                    title="Card title"
+                                    description="This is the description"
+                                />
+                            </Card>
+                        </List.Item>
+                    )}
+                />
+            </Card>
         </div>
     )
 };
