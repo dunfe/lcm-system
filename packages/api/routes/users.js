54fc0221e21b66ac526098967af1e61209ba2986
AnhTN <cachepdibao13@gmail.com>
2021-04-06 14:35:49 +0700

    add "report mentor from mentee" API

--- packages/api/routes/users.js
+++ packages/api/routes/users.js
@@ -5,11 +5,14 @@ import {ratingMentor} from '../controller/mentor.js';
 import {createQuestion, viewListQuestionMenteeId,viewListNewQuestionMenteeId, viewListDoingOrDoneQuestionMenteeId, getQuestionById, updateQuestionById, delQuestionById} from '../controller/question.js'
 import {getAllSkills} from '../controller/skill.js';
 import {viewPointInTransactionById, viewPointOutTransactionById } from "../controller/staff.js";
+import {registerMentorRequest} from '../controller/request.js';
+import { protect, restrictTo} from '../controller/auth.js';
+import { createReport } from '../controller/report.js';
+
 import passport from 'passport';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import {registerMentorRequest} from '../controller/request.js';
-import { protect, restrictTo} from '../controller/auth.js';
+
 dotenv.config();
 
 const router = express.Router();
@@ -244,6 +247,9 @@ router.post('/mentor/register',protect,restrictTo('mentee'),registerMentorReques
 
 //rate mentor
 router.post('/mentor/rate/:id',protect,restrictTo('mentee'),ratingMentor);
+
+//report mentor
+router.post('/reports', protect, restrictTo('mentee'), createReport);
 
 // user crud question
 router.post('/questions',protect,restrictTo('mentee'),createQuestion);
