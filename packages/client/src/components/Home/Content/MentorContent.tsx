8dd3f4eecd4709ae483fdccc83072c7e10568ed9
Dung <qngnud@gmail.com>
2021-04-08 11:03:22 +0700

    add menu for mentee, mentor

---
+++ packages/client/src/components/Home/Content/MentorContent.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import {Route, Switch} from "react-router-dom";
+import Dashboard from "../../../pages/Dashboard/Dashboard";
+import ListQuestion from "../../../pages/Question/ListQuestion";
+import Join from "../../Session/Join";
+import {Layout} from "antd";
+
+interface IProps {
+    path: string;
+}
+
+const {Content} = Layout;
+
+const MentorContent = (props: IProps) => {
+    const {path} = props;
+
+    return (
+        <Content style={{margin: '24px 16px 0'}}>
+            <Switch>
+                <Route exact path={path}>
+                    <Dashboard/>
+                </Route>
+                <div
+                    className="site-layout-background"
+                    style={{padding: 24, minHeight: 360, backgroundColor: "white"}}
+                >
+                    <Route path={`/questions`}>
+                        <ListQuestion/>
+                    </Route>
+                    <Route path={`/session`}>
+                        <Join/>
+                    </Route>
+                </div>
+            </Switch>
+        </Content>
+    )
+};
+
+export default MentorContent;
