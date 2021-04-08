2be92a0b44510800d6580e37a125db4918882974
Dung <qngnud@gmail.com>
2021-04-08 16:41:08 +0700

    update notify-server

--- packages/notify-server/app.js
+++ packages/notify-server/app.js
@@ -1,13 +1,16 @@
+const express = require("express");
 const cors = require('cors');
-var express = require("express");
-var app = express();
+const app = express();
+app.use(cors());
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 
-app.use(cors())
-var server = require("http").Server(app);
-var io = require("socket.io")(server);
-
-server.listen(3007);
+server.listen(3007, () => {
+    console.log('Server listening at port %d', 3007);
+});
 
 io.on('connection', (socket) => {
-    console.log(socket);
+    socket.on('news', data => {
+        socket.broadcast.emit('new', 5);
+    })
 });
