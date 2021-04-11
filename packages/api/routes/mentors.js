a2d6290355c3b4bb58d24f10cdbbd46541898735
Dat <datlthe130450@fpt.edu.vn>
2021-04-11 23:56:53 +0700

    update list notify

--- packages/api/routes/mentors.js
+++ packages/api/routes/mentors.js
@@ -7,8 +7,8 @@ import { protect, restrictTo} from '../controller/auth.js';
 import {getAllNotification, clickNotify} from '../controller/noti.js'
 const router = express.Router();
 
-router.get('/notify',protect, restrictTo('mentor'),getAllNotification);
-router.put('/notify/:id',protect, restrictTo('mentor'),clickNotify)
+// router.get('/notify',protect, restrictTo('mentor'),getAllNotification);
+// router.put('/notify/:id',protect, restrictTo('mentor'),clickNotify)
 router.get('/listQuestionForMentor',protect, restrictTo('mentor'),viewListQuestionForMentor)
 router.post('/selectQuestion/:id',protect, restrictTo('mentor'), selectQuestion)
 export default router;
