e9d7678c990edbd12c742bdff900c395796064cc
AnhTN <cachepdibao13@gmail.com>
2021-04-12 16:25:33 +0700

    fix cors policy

--- packages/api/app.js
+++ packages/api/app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods: GET, POST, DELETE, PUT');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control, Key, Access-Control-Allow-Origin, X-File-Name");
     res.header('Access-Control-Allow-Credentials: true');
     next();
 });
