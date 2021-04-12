fb31ab0343e3d7fdf160397e6bce23b395dfb29f
Dung <qngnud@gmail.com>
2021-04-12 22:13:36 +0700

    update cors

--- packages/api/controller/user.js
+++ packages/api/controller/user.js
@@ -240,7 +240,7 @@ export const viewUserInfo = async (req, res) => {
 }
 
 export const uploadAvatar = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
     res.setHeader('Access-Control-Allow-Credentials', true);
