cec0e7291b8913f679429e1a72ffc2e585a7cf15
Dat <datlthe130450@fpt.edu.vn>
2021-04-13 16:36:14 +0700

    add unit test of crud question

--- packages/api/test/user.js
+++ packages/api/test/user.js
@@ -112,6 +112,22 @@ describe('mentee crud question', () =>{
             })
     })
 
+    it('Should return message fail of list new and doing question without Authorization', function(done){
+        chai.request(app).get('/api/users/questions/new')
+            .end((err,res)=>{
+            expect(res.body.message).to.equal('Invalid Token. Maybe you are not logged in! Please log in to get acces or double check your token');
+            done();
+        })
+    })
+
+    it('Should return status fail of list new and doing question without Authorization', function(done){
+        chai.request(app).get('/api/users/questions/new')
+            .end((err,res)=>{
+            expect(res.body.status).to.equal('fail');
+            done();
+        })
+    })
+
     it('should return status of list done question ', (done) => {
         chai.request(app).get('/api/users/questions/done')
             .set('Authorization', token)
