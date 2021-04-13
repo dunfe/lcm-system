31852c8e6d476b14e05f3e403b20f1e391787cec
Dat <datlthe130450@fpt.edu.vn>
2021-04-13 18:42:09 +0700

    add unit test of question in role mentor

--- packages/api/test/user.js
+++ packages/api/test/user.js
@@ -624,7 +624,7 @@ describe('mentor crud question', () =>{
         })
     })
 
-    it('should return list new and doing question ', (done) => {
+    it('should return status success of list new and doing question ', (done) => {
         chai.request(app).get('/api/users/questions/new')
             .set('Authorization', token)
             .end((err,res) => {
@@ -632,6 +632,31 @@ describe('mentor crud question', () =>{
                 expect(res.body).to.contain.property('data');
                 done();
             })
+    })
+
+    it('should return data of list new and doing question', (done) => {
+        chai.request(app).get('/api/users/questions/new')
+            .set('Authorization', token)
+            .end((err,res) => {
+                expect(res.body).to.contain.property('data');
+                done();
+            })
+    })
+
+    it('Should return status fail of list new and doing question without Authorization', function(done){
+        chai.request(app).get('/api/users/questions/new')
+            .end((err,res)=>{
+            expect(res.body.status).to.equal('fail');
+            done();
+        })
+    })
+
+    it('Should return massage fail of list new and doing question without Authorization', function(done){
+        chai.request(app).get('/api/users/questions/new')
+            .end((err,res)=>{
+            expect(res.body.message).to.equal('Invalid Token. Maybe you are not logged in! Please log in to get acces or double check your token');
+            done();
+        })
     })
 
     it('should return list done question ', (done) => {
