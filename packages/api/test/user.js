e05cfeabb69499b0811cc32e6b3150a1f02cae4a
Dat <datlthe130450@fpt.edu.vn>
2021-04-13 20:23:43 +0700

    add unit test of question in role mentor

--- packages/api/test/user.js
+++ packages/api/test/user.js
@@ -709,6 +709,22 @@ describe('mentor crud question', () =>{
             done();
         })
     })
+
+    it('Should return status fail of question with id input without Authorization', function(done){
+        chai.request(app).get('/api/users/questions/done')
+            .end((err,res)=>{
+            expect(res.body.status).to.equal('fail');
+            done();
+        })
+    })
+
+    it('Should return massage fail of question with id input without Authorization', function(done){
+        chai.request(app).get('/api/users/questions/done')
+            .end((err,res)=>{
+            expect(res.body.message).to.equal('Invalid Token. Maybe you are not logged in! Please log in to get acces or double check your token');
+            done();
+        })
+    })
 })
 
 describe('rate mentor', () =>{
