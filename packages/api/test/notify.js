542674db99ff12d92425c9d520b7873fbdcda36d
Dat <datlthe130450@fpt.edu.vn>
2021-04-14 14:21:36 +0700

    unittest of notify

--- packages/api/test/notify.js
+++ packages/api/test/notify.js
@@ -67,5 +67,37 @@ describe(' notify of mentor', () =>{
         token = result.body.user.token;
       });
 
-    
+      it('Should return status success of list notify', function(done){
+        chai.request(app).get(`/api/users/notify`)
+            .set('Authorization', token)
+            .end((err,res)=>{
+            expect(res.body.status).to.equal('success');
+            done();
+        })
+    })
+
+    it('Should return data of list notify', function(done){
+        chai.request(app).get(`/api/users/notify`)
+            .set('Authorization', token)
+            .end((err,res)=>{
+            expect(res.body).to.contain.property('data');
+            done();
+        })
+    })
+
+    it('Should return status fail of list notify without Authorization', function(done){
+        chai.request(app).get(`/api/users/notify`)
+            .end((err,res)=>{
+            expect(res.body.status).to.equal('fail');
+            done();
+        })
+    })
+
+    it('Should return massage fail of list notify without Authorization', function(done){
+        chai.request(app).get(`/api/users/notify`)
+            .end((err,res)=>{
+            expect(res.body.message).to.equal('Invalid Token. Maybe you are not logged in! Please log in to get acces or double check your token');
+            done();
+        })
+    })
 })
