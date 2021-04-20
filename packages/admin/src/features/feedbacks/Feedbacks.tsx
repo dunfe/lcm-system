ef0241c33cae69509d0ea9290d6e0525619724a8
Dung <qngnud@gmail.com>
2021-04-20 14:01:43 +0700

    update feedback list

--- packages/admin/src/features/feedbacks/Feedbacks.tsx
+++ packages/admin/src/features/feedbacks/Feedbacks.tsx
@@ -1,27 +1,13 @@
 import * as React from 'react'
-import { Table, Space, Modal, Form, message } from 'antd'
+import { Table, Space } from 'antd'
 import axios from 'axios'
 import { useAuth } from '../../utils/hooks/useAuth'
-import { DeleteOutlined } from '@ant-design/icons'
-
-interface IProps {
-    onAdd: (state: any) => Promise<any>;
-    visible: boolean;
-    setVisible: (state: boolean) => void;
-}
 
 const { useState, useEffect } = React
-const { useForm } = Form
-const { confirm } = Modal
 
-const Feedbacks = (props: IProps) => {
-    const { setVisible } = props
+const Feedbacks = () => {
     const [data, setData] = useState([])
-    const [mode, setMode] = useState('add')
-    const [updateId, setUpdateId] = useState('')
-    const [itemDetail, setItemDetail] = useState({})
     const [current, setCurrent] = useState(1)
-    const [form] = useForm()
 
     const auth = useAuth()
     const instance = axios.create({
@@ -59,63 +45,25 @@ const Feedbacks = (props: IProps) => {
             key: 'action',
             render(text: string, record: any) {
                 return <Space size='middle' key={record._id}>
-                    <a onClick={() => onEdit(record._id)}>Edit</a>
-                    <a onClick={() => onDelete(record._id)}>Delete</a>
+                    <a onClick={() => onResolved(record._id)}>Resolved</a>
                 </Space>
             },
         },
     ]
 
-    const onDelete = (id: string) => {
-        confirm({
-            title: 'Xác nhận?',
-            icon: <DeleteOutlined />,
-            content: 'Hành động này không thể khôi phục',
-            onOk() {
-                instance.delete(`/api/admin/skills/${id}`).then((response) => {
-                    if (response.status === 200) {
-                        getData()
-                        message.success('Xoá thành công').then()
-                    }
-                }).catch((error) => message.error(error.message))
-            },
-            onCancel() {
-                console.log('Huỷ')
-            },
-        })
+    const onResolved = (id: string) => {
+        console.log(id)
     }
 
     const onPageChange = (page: number) => {
         setCurrent(page)
     }
 
-    const onEdit = (id: string) => {
-        setMode('update')
-        setUpdateId(id)
-        setVisible(true)
-    }
-
     const getData = () => {
         instance.get('/api/admin/reports').then((response) => {
-            setData(response.data.skill)
+            setData(response.data.data)
         }).catch((error) => console.error(error.message))
     }
-
-    useEffect(() => {
-        if (updateId !== '') {
-            instance.get(`/api/admin/skills/${updateId}`).then((response) => {
-                if (response.status === 200) {
-                    setItemDetail(response.data.data)
-                }
-            })
-        }
-    }, [updateId])
-
-    useEffect(() => {
-        if (mode === 'update') {
-            form.setFieldsValue(itemDetail)
-        }
-    }, [itemDetail])
 
     useEffect(() => {
         getData()
