f8e6ffd58e69329df6e842056f49b74f150485e9
Dung <qngnud@gmail.com>
2021-04-24 02:41:51 +0700

    update name long

--- packages/client/src/components/Auth/SignUpComponent.tsx
+++ packages/client/src/components/Auth/SignUpComponent.tsx
@@ -50,7 +50,7 @@ const SignUpComponent = () => {
             <Form.Item wrapperCol={{ span: 24 }} name="display_name">
                 <Input
                     width={'100%'}
-                    placeholder={t('Full name')}
+                    placeholder={t('Fullname')}
                     prefix={
                         <UserOutlined
                             className="site-form-item-icon"
@@ -65,8 +65,14 @@ const SignUpComponent = () => {
                 name="username"
                 rules={[
                     {
+                        pattern: new RegExp('(?=.{8,20}$)'),
+                        message: t(
+                            'Username must be between 8 and 20 character'
+                        ),
+                    },
+                    {
                         pattern: new RegExp(
-                            '^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$'
+                            '^(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$'
                         ),
                         message: t(
                             'Only use character or number in your username'
