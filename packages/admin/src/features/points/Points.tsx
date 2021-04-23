6545fcfd3c99a0da95236acd42f858151ee85a10
Dung <qngnud@gmail.com>
2021-04-23 16:13:21 +0700

    update viewpoint

--- packages/admin/src/features/points/Points.tsx
+++ packages/admin/src/features/points/Points.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react'
-import { Table, Space, Modal, Button, message, Tag } from 'antd'
-import { DeleteOutlined } from '@ant-design/icons'
+import { Table, Tag } from 'antd'
+import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons'
 import { useDispatch, useSelector } from 'react-redux'
 import { selectPoints, updatePoints } from './pointsSlice'
 import { useAPI } from '../../utils/hooks/useAPI'
 import { status } from '../../utils/status'
+import dayjs from 'dayjs'
 
 const { useState, useEffect } = React
-const { confirm } = Modal
 
 const Points = () => {
     const dispatch = useDispatch()
@@ -16,6 +16,10 @@ const Points = () => {
 
     const [current, setCurrent] = useState(1)
     const [total, setTotal] = useState(0)
+
+    const [expandId, setExpandId] = useState('')
+    const [pointOutData, setPointOutData] = useState([])
+    const [pointInData, setPointInData] = useState([])
 
     const columns = [
         {
@@ -55,37 +59,8 @@ const Points = () => {
                     }
                 })
             },
-        },
-        {
-            title: 'Hành động',
-            dataIndex: 'action',
-            key: 'action',
-            render(text: string, record: any) {
-                return <Space size='middle' key={record._id}>
-                    <Button danger onClick={() => onUpdate(record._id)}>Update</Button>
-                </Space>
-            },
-        },
+        }
     ]
-
-    const onUpdate = (id: string) => {
-        confirm({
-            title: 'Xác nhận?',
-            icon: <DeleteOutlined />,
-            content: 'Hành động này không thể khôi phục',
-            onOk() {
-                instance.delete(`/api/admin/requests/${id}`).then((response) => {
-                    if (response.status === 200) {
-                        getData()
-                        message.success('Xoá thành công').then()
-                    }
-                }).catch((error) => message.error(error.message))
-            },
-            onCancel() {
-                console.log('Huỷ')
-            },
-        })
-    }
 
     const onPageChange = (page: number) => {
         setCurrent(page)
@@ -98,6 +73,99 @@ const Points = () => {
         }).catch((error) => console.error(error.message))
     }
 
+    const expandedRowRender = () => {
+        const expandColumns = [
+            {
+                title: 'Time', dataIndex: 'createAt', key: 'createAt', render(text: string) {
+                    return dayjs(text).format('LLLL')
+                },
+            },
+            {
+                title: 'Amount', dataIndex: 'amount', key: 'amount', render(text: string) {
+                    return (
+                        <Tag>
+                            {text}
+                        </Tag>
+                    )
+                },
+            },
+            { title: 'Money', dataIndex: 'money', key: 'money' },
+            {
+                title: 'Status',
+                key: 'status',
+                dataIndex: 'status',
+            },
+            {
+                title: 'Before',
+                dataIndex: 'pointBefore',
+                key: 'pointBefore',
+            },
+            {
+                title: 'After',
+                dataIndex: 'pointAfter',
+                key: 'pointAfter',
+                render(text: string, record: any) {
+                    if (record.type === 'in') {
+                        return (
+                            <>
+                                <ArrowUpOutlined style={{ color: 'green' }} /> {text}
+                            </>
+                        )
+                    } else {
+                        return (
+                            <>
+                                <ArrowDownOutlined style={{ color: 'red' }} /> {text}
+                            </>
+                        )
+                    }
+                },
+            },
+            { title: 'Note', dataIndex: 'note', key: 'note' },
+        ]
+
+        const expandData = [...pointInData, ...pointOutData]
+
+        return <Table columns={expandColumns} dataSource={expandData} pagination={false} />
+    }
+
+    const onExpand = (expanded: boolean, record: any) => {
+        if (expanded && record._id) {
+            setExpandId(record._id)
+        }
+    }
+
+    useEffect(() => {
+        if (!expandId || expandId === '') {
+            return
+        }
+        const getExpandPointOutData = () => {
+            instance.get(`/api/staff/viewPointOut/${expandId}`).then((response) => {
+                const results = response.data.data.pointOutHistory.map((item: any) => {
+                    return {
+                        ...item,
+                        type: 'out',
+                    }
+                })
+                setPointOutData(results)
+            }).catch((error) => console.error(error.message))
+        }
+
+        const getExpandPointInData = () => {
+            instance.get(`/api/staff/viewPointIn/${expandId}`).then((response) => {
+                const results = response.data.data.pointInHistory.map((item: any) => {
+                    return {
+                        ...item,
+                        type: 'in',
+                    }
+                })
+                setPointInData(results)
+            }).catch((error) => console.error(error.message))
+        }
+
+        getExpandPointInData()
+        getExpandPointOutData()
+    }, [expandId])
+
     useEffect(() => {
         getData()
     }, [current])
@@ -105,6 +173,7 @@ const Points = () => {
     return (
         <>
             <Table columns={columns}
+                   expandable={{ expandedRowRender, onExpand }}
                    dataSource={data} rowKey={'_id'} pagination={{
                 current: current,
                 total,
