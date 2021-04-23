5fad7b584ce9037ca591135bbb0e5edc5b5893a1
Dung <qngnud@gmail.com>
2021-04-23 22:15:21 +0700

    update logo

--- packages/client/src/components/Logo/LogoContainer.tsx
+++ packages/client/src/components/Logo/LogoContainer.tsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
 
 export const LogoContainer = styled.div`
-    width: 100%;
+    padding-top: 12px;
+    width: 200px;
+    height: 100px;
     display: grid;
     place-items: center;
 `
