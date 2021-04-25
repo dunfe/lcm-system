200ed7c8dea14c0200e95b05ba5b505bcb6adf5c
Dat <datlthe130450@fpt.edu.vn>
2021-04-25 16:50:57 +0700

    view collab room by id

--- packages/api/routes/collabs.js
+++ packages/api/routes/collabs.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import colabRoom from '../models/collabRoom.js';
 import { protect, restrictTo} from '../controller/auth.js';
-import { createCollabRoom, joinRoomById,listRoom, endCollabRoomById} from '../controller/collabRoom.js'
+import { createCollabRoom, joinRoomById,listRoom, endCollabRoomById,viewCollabRoomById} from '../controller/collabRoom.js'
 
 const router = express.Router();
 
 router.get('/create-colab-room', protect, restrictTo('mentee', 'mentor'), createCollabRoom);
 router.get('/create-colab-room/:id', protect, restrictTo('mentee', 'mentor'), joinRoomById);
 router.get('/colab-room', protect, restrictTo('mentee', 'mentor'), listRoom);
-router.post('/colab-room/:id', protect, endCollabRoomById);
+router.post('/colab-room/:id', protect, restrictTo('mentee', 'mentor'), endCollabRoomById);
+router.get('/colab-room/:id', protect, restrictTo('mentee', 'mentor'), viewCollabRoomById);
 export default router;
