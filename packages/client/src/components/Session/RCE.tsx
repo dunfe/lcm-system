099348f5c6bd02ef7e82f37292f0d1a72fa7efe4
Dung <qngnud@gmail.com>
2021-05-06 01:42:43 +0700

    update editor

--- packages/client/src/components/Session/RCE.tsx
+++ packages/client/src/components/Session/RCE.tsx
@@ -5,6 +5,8 @@ import { WebsocketProvider } from 'y-websocket'
 import * as Y from 'yjs'
 import { MonacoBinding } from 'y-monaco'
 import Editor from '@monaco-editor/react'
+import { useState } from 'react'
+import { Select } from 'antd'
 
 interface IProps {
     id: string
@@ -12,9 +14,21 @@ interface IProps {
 const RCE = (props: IProps) => {
     const auth = useAuth()
     const { id } = props
+    const [language, setLanguage] = useState('Javascript')
 
     const ydoc = new Y.Doc()
     const provider = new WebsocketProvider('wss://y.livecoding.me', id, ydoc)
+
+    provider.on('change', (evt) => {
+        console.log(evt)
+        setLanguage(evt)
+    })
+
+    const onLanguageChange = (value) => {
+        provider.emit('change', [value])
+        setLanguage(value)
+    }
+
     const ytext = ydoc.getText('monaco')
 
     const awareness = provider.awareness
@@ -33,14 +47,64 @@ const RCE = (props: IProps) => {
         )
     }
 
+    const options = [
+        {
+            label: 'Javascript',
+            value: 'javascript',
+        },
+        {
+            label: 'Java',
+            value: 'java',
+        },
+        {
+            label: 'TypeScript',
+            value: 'typescript',
+        },
+        {
+            label: 'CSS',
+            value: 'css',
+        },
+        {
+            label: 'JSON',
+            value: 'json',
+        },
+        {
+            label: 'HTML',
+            value: 'html',
+        },
+        {
+            label: 'C#',
+            value: 'c#',
+        },
+        {
+            label: 'Python',
+            value: 'python',
+        },
+        {
+            label: 'PHP',
+            value: 'php',
+        },
+    ]
+
     return (
-        <Editor
-            onMount={handleEditorDidMount}
-            theme={'vs-dark'}
-            height="90vh"
-            defaultLanguage="javascript"
-            defaultValue="// some comment"
-        />
+        <>
+            <Editor
+                onMount={handleEditorDidMount}
+                theme={'vs-dark'}
+                height="90vh"
+                language={language}
+                defaultLanguage="javascript"
+                defaultValue="// some comment"
+            />
+            <Select
+                options={options}
+                style={{
+                    width: 120,
+                }}
+                value={language}
+                onChange={onLanguageChange}
+            />
+        </>
     )
 }
 
